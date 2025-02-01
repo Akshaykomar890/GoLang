a04bf76e9922cc9459e4e2f88e207f3ae2f1930f
Akshay Komar <akshaykomar732@gmail.com>
2025-02-01 14:20:27 +0530

    enums

---
+++ 18_enums/enums.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+//Enumerated Types
+
+// func changeOrder(status string){
+// 	fmt.Println("change to",status)
+// }
+
+// func main(){
+// 	changeOrder("received")
+// }
+
+//To Define a Enums using a Int
+// type Orders int
+
+// const (
+// 	Received Orders = iota
+// 	Updated
+// 	Confirmed
+// )
+
+// func getStatus(status Orders){
+// 	fmt.Println("Order Status",status)
+// }
+
+// func main(){
+// 	getStatus(Received)
+// 	getStatus(Confirmed)
+// }
+
+//To Define a Enums using a String Values
+//It helps in readability ,maintains a code structure
+type Orders string
+
+const (
+	Received Orders  = "Received"
+	Updated Orders   = "Updated"
+	Confirmed Orders = "Confirmed"
+)
+
+func getStatus(status Orders){
+	fmt.Println("Order Status",status)
+}
+
+func main(){
+	getStatus(Received)
+	getStatus(Confirmed)
+}
