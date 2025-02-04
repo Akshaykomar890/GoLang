4ce8a811afddd0313398e9d73b924888f46397be
Akshay Komar <akshaykomar732@gmail.com>
2025-02-04 13:21:27 +0530

    delete

--- 24_files/files.go
+++ 24_files/files.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
@@ -80,39 +79,52 @@ func main(){
 	// byte := []byte("hello go lang")
 	// file.Write(byte)
 
-	//stream get data and transform one to another
-	//read and write to another file
-	sourcefile,error:=os.Open("example.txt")
-	if error != nil{
-		panic(error)
-	}
-	defer sourcefile.Close()
+
+
+
+	//Stream get data and transform one to another
+	//Read and write to another file
+	// sourcefile,error:=os.Open("example.txt")
+	// if error != nil{
+	// 	panic(error)
+	// }
+	// defer sourcefile.Close()
 	
-	destfile,error:=os.Create("example2.txt")
-	if error != nil{
+	// destfile,error:=os.Create("example2.txt")
+	// if error != nil{
+	// 	panic(error)
+	// }
+
+	// defer destfile.Close()
+
+	// //stream of data
+	// //buffIo we use this
+	// //first read then write
+	// reader := bufio.NewReader(sourcefile) //sourcefile is also reader
+	// write :=bufio.NewWriter(destfile)
+
+	// for{
+	// 	b,error:=reader.ReadByte() //no byte avialble returns error
+	// 	if error != nil{ //error become false then loop will break
+	// 		if error.Error() != "EOF" {
+	// 			panic(error)	
+	// 		}
+	// 		break //then it stops infinite loop
+	// 	}
+	// 	write.WriteByte(b)
+	// }
+	// write.Flush()  //any data flush found last
+	// fmt.Println("Written new file")
+
+
+
+
+	//Delete file
+	error:=os.Remove("example.txt")
+	if error != nil {
 		panic(error)
 	}
-
-	defer destfile.Close()
-
-	//stream of data
-	//buffIo we use this
-	//first read then write
-	reader := bufio.NewReader(sourcefile) //sourcefile is also reader
-	write :=bufio.NewWriter(destfile)
-
-	for{
-		b,error:=reader.ReadByte()
-		if error != nil{
-			if error.Error() != "EOF" {
-				panic(error)	
-			}
-			break
-		}
-		write.WriteByte(b)
-	}
-	write.Flush()  //any data flush found last
-	fmt.Println("Written new file")
+	fmt.Println("file deleted")
 
 
 	
